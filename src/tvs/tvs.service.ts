bff0e2f5617fc8c87a6d2f3bc9182974ddfee93f
Nick <kolyabogach12@gmail.com>
2023-01-02 12:10:03 -0800

    env var fixes 2.2

--- src/tvs/tvs.service.ts
+++ src/tvs/tvs.service.ts
@@ -6,6 +6,8 @@ import { TvsRequestQueryDto } from './dto/request/tvs-request-query.dto';
 export class TvsService {
   // comments
   async getPopularTVs(query: TvsRequestQueryDto) {
+    console.log(process.env.PORT);
+    console.log(process.env.API_KEY);
     const popularTVs = await axios.get(
       `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}&language=en-US&page=${query.page}`,
     );
